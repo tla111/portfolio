f7bf726808cff63c484376005112977e3692c8e5
Tim La <email@example.com>
2020-11-15 20:09:26 -0500

    created and imported screens to app file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
+import Home from './screens/Home';
+import MyStory from './screens/MyStory';
+import MyWork from './screens/MyWork';
 
 function App() {
   return (
     <div className='App'>
-      <h1>Tim La's Portfolio</h1>
-      <h2>Testing Deployment</h2>
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route exact path='/about'>
+          <MyStory />
+        </Route>
+        <Route exact path='/work'>
+          <MyWork />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/screens/MyStory.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyStory = () => {
+  return (
+    <div>
+      <h1>My Story</h1>
+    </div>
+  );
+};
+
+export default MyStory;
