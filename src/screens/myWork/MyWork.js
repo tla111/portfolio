6d883cb72262f6d90607f3249240ed70594220f6
Tim La <email@example.com>
2020-11-18 10:13:17 -0500

    created door open animation

--- src/screens/myWork/MyWork.js
+++ src/screens/myWork/MyWork.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import Navigation from '../../components/navigation/Navigation';
+import './myWork.css';
 
 const MyWork = () => {
   return (
     <div>
       <Navigation />
-      <h1>My Work</h1>
+      <div className='work_container'>
+        <div className='gate_opener'>
+          <button className='open_gate'>Open</button>
+        </div>
+        <div className='left_door'></div>
+        <div className='right_door'></div>
+      </div>
     </div>
   );
 };
