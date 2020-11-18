68767e3e4483c48c53c4c591ab4df002cf8dad87
Tim La <email@example.com>
2020-11-17 19:22:36 -0500

    added communication comic to myStory section

--- src/screens/myStory/MyStory.js
+++ src/screens/myStory/MyStory.js
@@ -14,6 +14,7 @@ const MyStory = () => {
       <div className='comics_container'>
         <div className='leadership_comic'></div>
         <div className='ping_pong_comic'></div>
+        <div className='communication_comic'></div>
       </div>
     </div>
   );
