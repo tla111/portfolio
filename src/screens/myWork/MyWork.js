65626416dbac2734f9a4d2f8f94a527265ae1349
Tim La <email@example.com>
2020-11-18 13:17:35 -0500

    layout of containers set up

--- src/screens/myWork/MyWork.js
+++ src/screens/myWork/MyWork.js
@@ -9,6 +9,17 @@ const MyWork = () => {
       <div className='work_container'>
         <div className='left_door' id='l_door'></div>
         <div className='right_door' id='r_door'></div>
+        <div className='work_box work_box1'>
+          <div className='projects'></div>
+          <div className='projects'></div>
+          <div className='projects'></div>
+          <div className='projects'></div>
+          <div className='projects'></div>
+        </div>
+        <div className='work_box work_box2'></div>
+        <div className='work_box work_box3'></div>
+        <div className='work_box work_box4'></div>
+        <div className='work_box work_box5'></div>
       </div>
     </div>
   );
