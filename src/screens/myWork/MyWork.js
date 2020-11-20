8536068f0442fd1b658f0964e1a6371d6a2f100d
Tim La <email@example.com>
2020-11-19 21:13:15 -0500

    popup container is appearing correctly

--- src/screens/myWork/MyWork.js
+++ src/screens/myWork/MyWork.js
@@ -10,19 +10,19 @@ const MyWork = () => {
         <div className='left_door' id='l_door'></div>
         <div className='right_door' id='r_door'></div>
         <div className='work_box work_box1'>
-          <a href='https://www.google.com/'>
+          <a href='#popup1'>
             <div className='projects project_1'></div>
           </a>
-          <a href='https://www.google.com/'>
+          <a href='#popup1'>
             <div className='projects project_2'></div>
           </a>
-          <a href='https://www.google.com/'>
+          <a href='#popup1'>
             <div className='projects project_3'></div>
           </a>
-          <a href='https://www.google.com/'>
+          <a href='#popup1'>
             <div className='projects project_4'></div>
           </a>
-          <a href='https://www.google.com/'>
+          <a href='#popup1'>
             <div className='projects project_5'></div>
           </a>
         </div>
@@ -30,6 +30,19 @@ const MyWork = () => {
           <h1>Contact: tlamarketing111@gmail.com</h1>
         </div>
       </div>
+      {/* Popup 1 */}
+      <div className='popup' id='popup1'>
+        <div className='popup_container'>
+          <div className='popup_left'></div>
+          <div className='popup_right'>
+            <a href='#cardPage' className='popup_close'>
+              &times;
+            </a>
+            <div className='popup_right_container'></div>
+          </div>
+        </div>
+      </div>
+      {/* Popup 1 */}
     </div>
   );
 };
