65b85566a9f0ddc3d1d54b7a1f5468d4ee0ad92d
Tim La <email@example.com>
2020-12-10 20:50:43 -0500

    story page fully responsive

--- src/screens/myStory/MyStory.js
+++ src/screens/myStory/MyStory.js
@@ -7,6 +7,18 @@ import leader3 from './images/leadership/leader3.png';
 import leader4 from './images/leadership/leader4.png';
 import leader5 from './images/leadership/leader5.png';
 import leader6 from './images/leadership/leader6.png';
+import pong1 from './images/pingpong/pong1.png';
+import pong2 from './images/pingpong/pong2.png';
+import pong3 from './images/pingpong/pong3.png';
+import pong4 from './images/pingpong/pong4.png';
+import pong5 from './images/pingpong/pong5.png';
+import pong6 from './images/pingpong/pong6.png';
+import comm1 from './images/communication/comm1.png';
+import comm2 from './images/communication/comm2.png';
+import comm3 from './images/communication/comm3.png';
+import comm4 from './images/communication/comm4.png';
+import comm5 from './images/communication/comm5.png';
+import comm6 from './images/communication/comm6.png';
 
 const MyStory = () => {
   const leadership_comics = [
@@ -35,6 +47,58 @@ const MyStory = () => {
       image: leader6,
     },
   ];
+  const ping_pong_comics = [
+    {
+      id: 1,
+      image: pong1,
+    },
+    {
+      id: 2,
+      image: pong2,
+    },
+    {
+      id: 3,
+      image: pong3,
+    },
+    {
+      id: 4,
+      image: pong4,
+    },
+    {
+      id: 5,
+      image: pong5,
+    },
+    {
+      id: 6,
+      image: pong6,
+    },
+  ];
+  const communication_comics = [
+    {
+      id: 1,
+      image: comm1,
+    },
+    {
+      id: 2,
+      image: comm2,
+    },
+    {
+      id: 3,
+      image: comm3,
+    },
+    {
+      id: 4,
+      image: comm4,
+    },
+    {
+      id: 5,
+      image: comm5,
+    },
+    {
+      id: 6,
+      image: comm6,
+    },
+  ];
 
   return (
     <Container fluid>
@@ -44,13 +108,21 @@ const MyStory = () => {
         <h1>PERSISTENCE</h1>
       </Row>
       <Row className='comics_container'>
-        <div className='leadership_container'>
+        <div className='comic_container'>
           {leadership_comics.map((leader) => (
-            <img src={leader.image} key={leader.id} alt='leader' />
+            <img src={leader.image} key={leader.id} alt='leadership comic' />
           ))}
         </div>
-        {/* <div className='ping_pong_comic'></div>
-        <div className='communication_comic'></div> */}
+        <div className='comic_container'>
+          {ping_pong_comics.map((pong) => (
+            <img src={pong.image} key={pong.id} alt='ping pong comic' />
+          ))}
+        </div>
+        <div className='comic_container'>
+          {communication_comics.map((comm) => (
+            <img src={comm.image} key={comm.id} alt='communication comic' />
+          ))}
+        </div>
       </Row>
     </Container>
   );
