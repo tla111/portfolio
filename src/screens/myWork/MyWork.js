ed5e72050f41b0a84fdc55b0360eba00e982f299
Tim La <email@example.com>
2020-12-31 18:36:29 -0500

    card for work-it-out project completed

--- src/screens/myWork/MyWork.js
+++ src/screens/myWork/MyWork.js
@@ -114,9 +114,9 @@ const MyWork = () => {
       {/* Popup 2 */}
       {/* Popup 3 */}
       <Container fluid className='popup' id='popup3'>
-        <Row className='popup_container'>
+        <Row className='popup_container workout_container'>
           <Col className='project_info'>
-            <div className='info_section1'>
+            {/* <div className='info_section1'>
               <p>Work It Out 2020</p>
             </div>
             <div className='info_section2'>
@@ -136,8 +136,45 @@ const MyWork = () => {
               >
                 <button className='popup_but'>Code</button>
               </a>
+            </div> */}
+            <div className='info_section1 workout_section'>
+              <h3 class='card-header'>Work-It-Out</h3>
+              <div class='card-body'>
+                <p class='card-text'>
+                  ▪️{''} Gives the user self-accountability to exercise and
+                  workout by giving them a platform to keep track of their body
+                  mass index and fitness goals
+                </p>
+                <p class='card-text'>
+                  ▪️{''} The web application comprises of a login system, the
+                  use of CRUD operations, functionality of a social media
+                  platform, and features of a bug tracker application
+                </p>
+              </div>
+              <div class='card-body'>
+                <h5 class='card-title'>Technologies:</h5>
+                <h6 class='card-subtitle text-muted'>
+                  Django | Python | JavaScript | SQL | Bootstrap & CSS for UI
+                  Design | Heroku
+                </h6>
+              </div>
+              <div className='popup_buttons'>
+                <a
+                  href='https://work-it-out-2020.herokuapp.com/'
+                  target='_blank'
+                >
+                  <button className='popup_but workout_btn'>Website</button>
+                </a>
+                <a
+                  href='https://github.com/tla111/Work-It-Out-2020'
+                  target='_blank'
+                >
+                  <button className='popup_but workout_btn'>Code</button>
+                </a>
+              </div>
             </div>
           </Col>
+
           <a href='#work' className='popup_close'>
             &times;
           </a>
@@ -147,23 +184,6 @@ const MyWork = () => {
       {/* Popup 4 */}
       <Container fluid className='popup' id='popup4'>
         <Row className='popup_container'>
-          {/* <Col className='project_info'>
-            <div className='info_section1'>
-              <p>International Space Station (ISS)</p>
-            </div>
-            <div className='info_section2'>
-              <h3>
-                Front End: <span>React</span>
-              </h3>
-            </div>
-          </Col>
-          <Col>
-            <div className='popup_buttons'>
-              <a href='https://github.com/tla111/se-q3-iss' target='_blank'>
-                <button className='popup_but'>Code</button>
-              </a>
-            </div>
-          </Col> */}
           <Col className='project_info'>
             <div className='info_section1'>
               <h3 class='card-header'>International Space Station</h3>
